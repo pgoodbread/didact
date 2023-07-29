b172ce91f9f1d07926074cceedc3b3aeab7554d9
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2023-07-29 16:40:26 +0200

    implement the render function

--- didact.js
+++ didact.js
@@ -1,17 +1,12 @@
-// ------ GLOBAL CODE ------
-const Didact = {
-  createElement,
-};
-
 // children is always an array
 function createElement(type, props, ...children) {
   return {
     type,
     props: {
       ...props,
-      children: children.map((child) => {
-        typeof child == "object" ? child : createTextElement(child);
-      }),
+      children: children.map((child) =>
+        typeof child === "object" ? child : createTextElement(child)
+      ),
     },
   };
 }
@@ -27,12 +22,33 @@ function createTextElement(text) {
   };
 }
 
+function render(element, container) {
+  const domNode =
+    element.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(element.type);
+  element.props.children.forEach((child) => render(child, domNode));
+
+  // apply all props that are not children to domNode
+  const isNotChildren = (key) => key !== "children";
+  Object.keys(element.props)
+    .filter(isNotChildren)
+    .forEach((prop) => (domNode[prop] = element.props[prop]));
+
+  container.appendChild(domNode);
+}
+
+const Didact = {
+  createElement,
+  render,
+};
+
 /** @jsx Didact.createElement */
 const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
+  <div style="background: salmon">
+    <h1>Hello Philip</h1>
+    <h2 style="text-align:right">from Didact</h2>
   </div>
 );
 const container = document.getElementById("root");
-ReactDOM.render(element, container);
+Didact.render(element, container);
