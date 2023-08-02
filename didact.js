52453c802ae1d9c675eaaacea4a70685dabd3384
Philip Gutbrodt <philip.gutbrodt@axelspringer.com>
2023-08-02 20:51:15 +0200

    finish adding hooks (step 8)

--- didact.js
+++ didact.js
@@ -160,11 +160,44 @@ function performUnitOfWork(fiber) {
     currentFiber = currentFiber.parent;
   }
 }
+let wipFiber = null;
+let hookIndex = null;
 
 function updateFunctionComponent(fiber) {
+  wipFiber = fiber;
+  hookIndex = 0;
+  wipFiber.hooks = [];
   // run function component function to get children
   const children = [fiber.type(fiber.props)];
   reconcileChildren(fiber, children);
+}
+
+function useState(initial) {
+  const oldHook =
+    wipFiber.alternate &&
+    wipFiber.alternate.hooks &&
+    wipFiber.alternate.hooks[hookIndex];
+  const hook = {
+    state: oldHook ? oldHook.state : initial,
+    queue: [],
+  };
+  const actions = oldHook ? oldHook.queue : [];
+  actions.forEach((action) => {
+    hook.state = action(hook.state);
+  });
+  const setState = (action) => {
+    hook.queue.push(action);
+    wipRoot = {
+      domNode: currentRoot.domNode,
+      props: currentRoot.props,
+      alternate: currentRoot,
+    };
+    nextUnitOfWork = wipRoot;
+    deletions = [];
+  };
+  wipFiber.hooks.push(hook);
+  hookIndex++;
+  return [hook.state, setState];
 }
 
 function updateHostComponent(fiber) {
@@ -240,12 +273,18 @@ function reconcileChildren(wipFiber, children) {
 const Didact = {
   createElement,
   render,
+  useState,
 };
 
 /** @jsx Didact.createElement */
-function App(props) {
-  return <h1>Hi {props.name}</h1>;
+function Counter(props) {
+  const [state, setState] = Didact.useState(1);
+  return (
+    <h1 onClick={() => setState((c) => c + 1)}>
+      Countof {props.name}: {state}
+    </h1>
+  );
 }
-const element = <App name="foo" />;
+const element = <Counter name="plätzchen" />;
 const container = document.getElementById("root");
 Didact.render(element, container);
